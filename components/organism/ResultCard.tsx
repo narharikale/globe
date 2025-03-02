0d3f49bd99952e862172225fb8504db5cbaf7f11
narharikale <n7kale@gmail.com>
2025-03-02 11:21:32 +0530

    fix: Remove incorrect answer display in ResultCard

--- components/organism/ResultCard.tsx
+++ components/organism/ResultCard.tsx
@@ -74,7 +74,7 @@ export const ResultCard: React.FC<ResultCardProps> = ({
           <p className="text-gray-600 mb-4">
             {isCorrect 
               ? `You nailed it! ${destination} is the answer.` 
-              : `The correct answer was ${destination}.`}
+              : null}
           </p>
           
           <div className="bg-white rounded-lg p-4 w-full mb-6">
